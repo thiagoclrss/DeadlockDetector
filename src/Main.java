8ab8dbf14d619cef6de15ce2e6c23ac580c2c0ef
João Victor Meireles Vieira <68872871+JVictorMeireles@users.noreply.github.com>
2023-06-05 18:47:58 -0300

    Inserindo funções de grafos

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,62 @@
 import java.util.Scanner;
 
-public class Main {
-	public static void main(String[] args) {
+public class Main //Classe principal
+{
+	public static void main(String[] args) //Função principal
+	{
 		Scanner n = new Scanner(System.in);
-		System.out.println("informe o intervalo de tempo de verificação: ");
+
+		createOS(n);
+		int resourcesQuantity = resourceParameters(n);
+
+		System.out.println("Informe a quantidade de processos: ");
+		int proccessQuantity = n.nextInt();
+
+		Graph graph = new Graph(proccessQuantity);
+		for (int i = 0; i < resourcesQuantity; i++)
+		{
+			System.out.println("Informe o remetente da aresta direcionada: ");
+			int edgeSource = n.nextInt();
+			System.out.println("Informe o destinatário da aresta direcionada: ");
+			int edgeDest = n.nextInt();
+			graph.addEdge(edgeSource, edgeDest);
+		}
+		/*
+		// Function call
+		if(graph.isCyclic())
+			System.out.println("Graph contains cycle");
+		else
+			System.out.println("Graph doesn't "
+					+ "contain cycle");*/
+	}
+	private static int resourceParameters(Scanner n)
+	{
+		int resourcesQuantity = 0;
+		while(resourcesQuantity>10 || resourcesQuantity<1) //Valor mínimo: 1; Valor máximo: 10;
+		{
+			System.out.println("Informe a quantidade de recursos (digite um valor entre 1 e 10): ");
+			resourcesQuantity = n.nextInt();
+		}
+		for (int i=0; i<resourcesQuantity;i++) //Para cada recurso instanciado, é pedido um nome
+		{
+			System.out.println("Informe o nome do recurso: ");
+			String resourceName = n.next();
+			Resource resource = new Resource(resourceName, i+1, 1);
+		}
+		return resourcesQuantity;
+	}
+
+	private static void createOS(Scanner n)
+	{
+		System.out.println("Informe o intervalo de tempo de verificação: ");
 		int deltaTimeDeadLock = n.nextInt();
-		
+
 		OperationalSystem os = new OperationalSystem(deltaTimeDeadLock);
 		os.timer();
 		System.out.println("");
-		
 	}
 }
+
 
 /*
 
